0ced46e2fc192347a9041e5b4c077d0d1bb6827d
Carlos2607-a <Carlos.mijares0993@gmail.com>
2025-02-28 21:57:44 +0100

    act

--- Home.py
+++ Home.py
@@ -4,5 +4,9 @@ import pandas as pd
 st.title("Gestión Luna Beach")
 st.write("Bienvenido a la página principal de Gestrest2.0")
 
-st.text_input("Introduce tu nombre")
-st.password_input("Introduce tu contraseña")
+name = st.text_input("Introduce tu nombre")
+
+password = st.password_input("Introduce tu contraseña")
+
+st.write(f"Nombre ingresado: {name}")
+st.write(f"Contraseña ingresada: {password}")
