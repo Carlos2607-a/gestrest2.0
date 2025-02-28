e541db20923a32fade7c375f496bb29e3332be9b
Carlos2607-a <Carlos.mijares0993@gmail.com>
2025-02-28 22:23:04 +0100

    act

--- Home.py
+++ Home.py
@@ -12,9 +12,8 @@ EMPLOYEES = {
     "Neiser": "123456"
 }
 
-# Leer el DataFrame
 try:
-    data = pd.read_csv("data.csv", index_col=0) #se agrega index_col=0, para que la primera columna sea el index.
+    data = pd.read_csv("data.csv", index_col=0)
 except FileNotFoundError:
     data = pd.DataFrame(columns=list(EMPLOYEES.keys()))
 
@@ -26,18 +25,18 @@ def check_authentication():
         if user == ADMIN_USER and password == ADMIN_PASSWORD:
             st.session_state["authenticated"] = True
             st.session_state["role"] = "admin"
-            st.session_state["user"] = user #agregamos el usuario al estado de la session.
+            st.session_state["user"] = user
         elif user in EMPLOYEES and password == EMPLOYEES[user]:
             st.session_state["authenticated"] = True
             st.session_state["role"] = "employee"
-            st.session_state["user"] = user #agregamos el usuario al estado de la session.
+            st.session_state["user"] = user
         else:
             st.error("Credenciales incorrectas")
 
 if "authenticated" not in st.session_state:
     st.session_state["authenticated"] = False
     st.session_state["role"] = None
-    st.session_state["user"] = None #agregamos el usuario al estado de la session.
+    st.session_state["user"] = None
 
 if st.session_state["authenticated"]:
     if st.session_state["role"] == "admin":
@@ -55,10 +54,10 @@ if st.session_state["authenticated"]:
                     data.loc[fecha.strftime("%Y-%m-%d"), empleado] += horas_extras
                 else:
                    data.loc[fecha.strftime("%Y-%m-%d"), empleado] = horas_extras
-                data.to_csv("data.csv")  # Guardar los cambios
-                st.success("Horas extras agregadas correctamente.")
-                st.dataframe(data)
             else:
                 st.error("Empleado no encontrado en el DataFrame.")
+            data.to_csv("data.csv")  # Guardar los cambios
+            st.success("Horas extras agregadas correctamente.")
+            st.dataframe(data)
 else:
     check_authentication()
