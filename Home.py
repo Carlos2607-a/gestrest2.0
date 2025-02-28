403f5a436c9d6760280317224fabc3b8f10a649b
Carlos2607-a <Carlos.mijares0993@gmail.com>
2025-02-28 21:12:11 +0100

    act

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Home")
+st.write("Bienvenido a la página principal de Gestrest2.0")
+
+st.subheader("Descripción")
+st.write("Gestrest2.0 es una aplicación web que permite a los usuarios gestionar sus tareas de manera fácil y eficiente.")
