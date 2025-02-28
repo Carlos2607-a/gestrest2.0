22fa64c0500137c3dd85d718b5c35c0e6e7863cc
Carlos2607-a <Carlos.mijares0993@gmail.com>
2025-02-28 21:55:05 +0100

    act

--- Home.py
+++ Home.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Home")
+st.title("Gestión Luna Beach")
 st.write("Bienvenido a la página principal de Gestrest2.0")
 
-st.subheader("Descripción")
-st.write("Gestrest2.0 es una aplicación web que permite a los usuarios gestionar sus tareas de manera fácil y eficiente.")
+st.text_input("Introduce tu nombre")
+st.password_input("Introduce tu contraseña")
